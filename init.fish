eddbaec286f8fd41322dbef9e99923e404f433f8
mhv2109 <marshall.vanloon@gmail.com>
2021-04-08 20:31:08 -0400

    update init.fish for elixir

--- init.fish
+++ init.fish
@@ -1,5 +1,9 @@
+# load bash profiles
 if test -d /etc/profile.d
     for file in /etc/profile.d/*.sh
         bass source $file
     end
 end
+
+# Elixir & Erlang
+set -Ux ERL_AFLAGS "-kernel shell_history enabled"
