b9c14c3a768c4d39d56aff0881409c64d84b9407
mhv2109 <marshall.vanloon@gmail.com>
2021-04-27 09:34:12 -0400

    add pyenv and pyenv-virtualenv to init

--- init.fish
+++ init.fish
@@ -7,3 +7,11 @@ end
 
 # Elixir & Erlang
 set -Ux ERL_AFLAGS "-kernel shell_history enabled"
+
+# pyenv and pyenv-virtualenv init (if installed)
+if test -x (which pyenv)
+    status --is-interactive; and pyenv init - | source
+    if test -d (pyenv root)/plugins/pyenv-virtualenv
+        status --is-interactive; and pyenv virtualenv-init - | source
+    end
+end
